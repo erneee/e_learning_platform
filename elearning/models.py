049770afae6c9df51a6990c3d8ff77fd64db5d89
Ernestas <ernestasvvv@gmail.com>
2023-08-29 09:05:09 +0300

    Vietoj user pakeista i student

--- elearning/models.py
+++ elearning/models.py
@@ -1,21 +1,21 @@
 from django.db import models
 
 
-class User(models.Model):
-    username = models.CharField(max_length=20)
-    password = models.CharField(max_length=20)
-    email = models.EmailField(max_length=254, unique=True)
+class Student(models.Model):
+    f_name = models.CharField(max_length=20)
+    l_name = models.CharField(max_length=20)
+    age = models.IntegerField(max_length=3)
+    # email = models.EmailField(max_length=254, unique=True)
 
 
 class Course(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
-    instructor = models.ForeignKey(User, on_delete=models.CASCADE)
-    students = models.ManyToManyField(User, through='Enrollment')
+    students = models.ManyToManyField(Student, through='Enrollment')
 
 
 class Enrollment(models.Model):
-    student = models.ForeignKey(User, on_delete=models.CASCADE)
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
     score = models.DecimalField(max_digits=5, decimal_places=2)
     date = models.DateTimeField(auto_now_add=True)
@@ -23,7 +23,7 @@ class Enrollment(models.Model):
 
 
 class Submission(models.Model):
-    student = models.ForeignKey(User, on_delete=models.CASCADE)
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
     assignment = models.ForeignKey('Assignment', on_delete=models.CASCADE)
     submission_date = models.DateTimeField(auto_now_add=True)
 
--- elearning/views.py
+++ elearning/views.py
@@ -3,6 +3,6 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from django.views import generic
 from django.db.models import Q
-from .models import Course, User, Assignment, Submission, Enrollment
+from .models import Course, Student, Assignment, Submission, Enrollment
 
 
